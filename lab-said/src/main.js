0aa37bc99f861ac3c81b7202aec3a3bd59dbd638
saidmattar <ssabdou@gmail.com>
2017-09-25 16:38:24 -0700

    got mu app working with a little css for now

---
+++ lab-said/src/main.js
@@ -0,0 +1,36 @@
+import './styles/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+import faker from 'faker';
+import cowsay from 'cowsay-browser';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState(prevState => {
+      return {
+        content: cowsay.say({text: faker.lorem.words(20)}),
+      };
+    }
+    );
+  }
+
+  render(){
+    return (
+      <div>
+        <h1> Generate Cowsay Lorem </h1>
+        <button onClick={this.handleClick}> click here! </button>
+         <pre> {this.state.content} </pre>
+      </div>
+    );
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
