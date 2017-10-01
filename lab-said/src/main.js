0727016af82af32bbdf7af64e5338b42b7009277
saidmattar <ssabdou@gmail.com>
2017-10-01 15:41:39 -0700

    got a dropdown containing a list of different options to choose from ....!

--- lab-said/src/main.js
+++ lab-said/src/main.js
@@ -8,24 +8,41 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: '',
+      cows:[],
+      current:'',
+      content: cowsay.say({text: 'click the button!'}),
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e) {
-    this.setState(prevState => {
-      return {
-        content: cowsay.say({text: faker.lorem.words(20),f: 'dragon'}),
-      };
+  componentWillMount(){
+    cowsay.list((err, cows) =>{
+      let current=cows[0];
+      this.setState({ cows: cows, current:current});
     }
-    );
+);
+
   }
+
+  handleClick(e) {
+    let current=e.target.value;
+    let text = faker.hacker.phrase();
+    this.setState({current,
+      content: cowsay.say({text,f: current})});
+  }
+
+
 
   render(){
     return (
       <div>
         <h1> Generate Cowsay Lorem </h1>
+
+        <select onChange={this.handleClick}>
+        {this.state.cows.map((cow, i)=> {
+          return <option key={i} value={cow}>{cow}</option>;
+        }
+      )}</select>
         <button onClick={this.handleClick}> click here! </button>
          <pre> {this.state.content} </pre>
       </div>
